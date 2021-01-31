758b96aa84d50aac2d6f046124c98bac94cbfc04
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2021-02-01 01:26:13 +0200

    Intial commit

---
+++ lib/test_repo_example.ex
@@ -0,0 +1,18 @@
+defmodule TestRepoExample do
+  @moduledoc """
+  Documentation for `TestRepoExample`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TestRepoExample.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/test_repo_example_test.exs
@@ -0,0 +1,20 @@
+defmodule TestRepoExampleTest do
+  use ExUnit.Case
+  doctest TestRepoExample
+
+  test "sort 3 elem list" do
+    assert CodeRepoExample.sort([2, 2, 1]) == [1, 2, 2]
+  end
+
+  test "sort 4 elem list" do
+    assert CodeRepoExample.sort([2, 5, 2, 1]) == [1, 2, 2, 5]
+  end
+
+  test "sort 5 elem list" do
+    assert CodeRepoExample.sort([-1, 5, 2, 2, 1]) == [-1, 1, 2, 2, 5]
+  end
+
+  test "sort 6 elem list" do
+    assert CodeRepoExample.sort([-1, 5, 2, 2, 1, 0]) == [-1, 0, 1, 2, 2, 5]
+  end
+end
